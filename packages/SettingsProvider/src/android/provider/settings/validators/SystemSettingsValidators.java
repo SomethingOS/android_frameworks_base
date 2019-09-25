050c2d0a8d8d3c34c169b76c73abdf79ad096471
maxwen <max.weninger@gmail.com>
2019-09-25 16:19:49 +0200

    base: Back gesture height settings [1/2]
    idoybh (YAAP):
    * Adapted
    * Improved code and logic
    * Improved docs, typos etc
    * Use sixths instead of fixed values, add 2 more steps
    
    Change-Id: I6ebe0039e250f3b8f0b91b232df5941b58026499
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
@@ -233,5 +233,6 @@ public class SystemSettingsValidators {
         VALIDATORS.put(System.SCREEN_FLASH_NOTIFICATION_COLOR, ANY_INTEGER_VALIDATOR);
         VALIDATORS.put(System.RINGTONE_VIBRATION_PATTERN, new InclusiveIntegerRangeValidator(0, 4));
         VALIDATORS.put(System.NOTIFICATION_VIBRATION_PATTERN, new InclusiveIntegerRangeValidator(0, 4));
+        VALIDATORS.put(System.BACK_GESTURE_HEIGHT, new InclusiveIntegerRangeValidator(0, 5));
     }
 }
