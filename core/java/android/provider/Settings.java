16787f35920c1e1c453451292c062c3b6f3e6095
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-11-30 13:41:05 +0100

    SystemUI: Bring back good ol' circle battery style
    Change-Id: If9c55253c8e8786b4998632241a872104b7e7e8e
    Co-Authored-By: althafvly <althafvly@gmail.com>
    Co-Authored-By: Bruno Martins <bgcngm@gmail.com>
    Co-Authored-By: Michael Bestas <mkbestas@lineageos.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5188,6 +5188,26 @@ public final class Settings {
         public static final String DC_DIMMING_STATE = "dc_dimming_state";
 
         /**
+         * Display style of the status bar battery information
+         * 0: Display the battery an icon in portrait mode
+         * 1: Display the battery as a circle
+         * 2: Display the battery as plain text
+         * default: 0
+         * @hide
+         */
+        public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+
+        /**
+         * Status bar battery %
+         * 0: Hide the battery percentage
+         * 1: Display the battery percentage next to the icon
+         * 2: Display the battery percentage inside the icon
+         *
+         * @hide
+         */
+        public static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_battery_percent";
+
+        /**
          * Control whether to enable adaptive sleep mode.
          * @deprecated Use {@link android.provider.Settings.Secure#ADAPTIVE_SLEEP} instead.
          * @hide
