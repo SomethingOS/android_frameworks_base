09e69ed0a12a63cd256402aea175dde61c7ed15f
jhonboy121 <alfredmathew05@gmail.com>
2022-01-11 20:43:24 +0530

    Allow disabling qs on secure lockscreen
    * This does not prevent pulling down notification shade,
      qs will be entirely hidden even if you do and that's it.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6051,6 +6051,12 @@ public final class Settings {
          */
         @Readable
         public static final String NOTIFICATION_VIBRATION_PATTERN = "notification_vibration_pattern";
+         
+        /**
+         * Whether to disable qs on secure lockscreen.
+         * @hide
+         */
+        public static final String SECURE_LOCKSCREEN_QS_DISABLED = "secure_lockscreen_qs_disabled";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
