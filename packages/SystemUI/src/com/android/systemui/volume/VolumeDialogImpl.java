b056db2daccad966dc6250f2ba08a658dca40844
Arian <arian.kulmer@web.de>
2024-01-27 19:07:00 +0100

    fixup! VolumeDialogImpl: Ignore external layout direction changes

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -2820,7 +2820,6 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
 
         @Override
         public void onLayoutDirectionChanged(int layoutDirection) {
-            onConfigurationChanged();
         }
 
         @Override
