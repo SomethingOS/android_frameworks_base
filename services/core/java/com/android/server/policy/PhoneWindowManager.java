ccf0706493bca2c53d1f15a345ac8975e32aef1f
Fazil Sheik <fazil.sheik96@gmail.com>
2024-09-12 16:34:53 +0530

    base: Export Receiver for PhoneWindowManager
    Required for power menu broadcast after new Dolby Implementation.
    
    --------- beginning of crash
    09-11 19:44:41.119  1924  1953 E AndroidRuntime: *** FATAL EXCEPTION IN SYSTEM PROCESS: android.ui
    09-11 19:44:41.119  1924  1953 E AndroidRuntime: java.lang.SecurityException: android: One of RECEIVER_EXPORTED or RECEIVER_NOT_EXPORTED should be specified when a receiver isn't being registered exclusively for system broadcasts
    09-11 19:44:41.119  1924  1953 E AndroidRuntime: 	at com.android.server.am.ActivityManagerService.registerReceiverWithFeature(ActivityManagerService.java:14125)
    09-11 19:44:41.119  1924  1953 E AndroidRuntime: 	at android.app.ContextImpl.registerReceiverInternal(ContextImpl.java:1852)
    09-11 19:44:41.119  1924  1953 E AndroidRuntime: 	at android.app.ContextImpl.registerReceiver(ContextImpl.java:1792)
    09-11 19:44:41.119  1924  1953 E AndroidRuntime: 	at android.app.ContextImpl.registerReceiver(ContextImpl.java:1780)
    09-11 19:44:41.119  1924  1953 E AndroidRuntime: 	at com.android.server.policy.PhoneWindowManager.init(PhoneWindowManager.java:2403)
    09-11 19:44:41.119  1924  1953 E AndroidRuntime: 	at com.android.server.policy.PhoneWindowManager.init(PhoneWindowManager.java:2194)
    09-11 19:44:41.119  1924  1953 E AndroidRuntime: 	at com.android.server.wm.WindowManagerService$5.run(WindowManagerService.java:1146)
    09-11 19:44:41.119  1924  1953 E AndroidRuntime: 	at android.os.Handler$BlockingRunnable.run(Handler.java:984)
    09-11 19:44:41.119  1924  1953 E AndroidRuntime: 	at android.os.Handler.handleCallback(Handler.java:958)
    09-11 19:44:41.119  1924  1953 E AndroidRuntime: 	at android.os.Handler.dispatchMessage(Handler.java:99)
    09-11 19:44:41.119  1924  1953 E AndroidRuntime: 	at android.os.Looper.loopOnce(Looper.java:205)
    09-11 19:44:41.119  1924  1953 E AndroidRuntime: 	at android.os.Looper.loop(Looper.java:294)
    09-11 19:44:41.119  1924  1953 E AndroidRuntime: 	at android.os.HandlerThread.run(HandlerThread.java:67)
    09-11 19:44:41.119  1924  1953 E AndroidRuntime: 	at com.android.server.ServiceThread.run(ServiceThread.java:46)
    09-11 19:44:41.119  1924  1953 E AndroidRuntime: 	at com.android.server.UiThread.run(UiThread.java:45)
    
    Change-Id: Id7ca50784b2f6a843dcf9d246fe56d1210bfd07c
    Signed-off-by: Fazil Sheik <fazil.sheik96@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -2517,7 +2517,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             public void onReceive(Context context, Intent intent) {
                 showGlobalActionsInternal();
             }
-        }, filter);
+        }, filter, Context.RECEIVER_EXPORTED);
 
         mVibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
         mHapticFeedbackVibrationProvider =
